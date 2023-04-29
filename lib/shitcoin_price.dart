4c8bd44f92f067911af553e68974298e26ba87e4
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-04-29 03:41:11 +0000

    added abi

--- lib/shitcoin_price.dart
+++ lib/shitcoin_price.dart
@@ -2,33 +2,19 @@
 
 library shitcoin_price;
 
+import 'package:flutter/services.dart';
 import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart';
 
 void main() async {
-
   const Token = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
-  final abi = [
-    {
-      "inputs": [
-        {"internalType": "uint256", "name": "amountIn", "type": "uint256"},
-        {"internalType": "address[]", "name": "path", "type": "address[]"}
-      ],
-      "name": "getAmountsOut",
-      "outputs": [
-        {"internalType": "uint256[]", "name": "amounts", "type": "uint256[]"}
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+  final abi = await rootBundle.loadString('assets/router.json');
 
   final httpClient = Client();
   final ethClient =
       Web3Client('https://bsc-dataseed1.binance.org:443', httpClient);
   final contract = DeployedContract(
-      ContractAbi.fromJson(
-          abi as String, '0x10ed43c718714eb63d5aa57b78b54704e256024e'),
+      ContractAbi.fromJson(abi, '0x10ed43c718714eb63d5aa57b78b54704e256024e'),
       EthereumAddress.fromHex('0x10ed43c718714eb63d5aa57b78b54704e256024e'));
   final result = await ethClient.call(
       contract: contract,
